e3555a57e73fe781cd7e3d2e1da1049b88c73b83
unknown <youngerous@gmail.com>
2020-10-16 22:22:35 +0900

    regression

--- regression.py
+++ regression.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-import time
 import random
+import time
 
 import numpy as np
 import torch
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 
-from model import ResNet
+from model import ResNet34
 from trainer import Trainer
 from utils.build_dataloader import get_dataloader
 
@@ -33,37 +33,39 @@ def main(args):
         "momentum": args.momentum,
         "weight_decay": args.weight_decay,
         "optimizer": args.optimizer,
-        "criterion": nn.CrossEntropyLoss(),
+        "criterion": nn.MSELoss(),
         "eval_step": args.eval_step,
     }
 
     fix_seed(args.seed)
-    model = ResNet()  # TODO: num_classes 추가
+    model = ResNet34(num_classes=args.num_classes)
     trainer = Trainer(model=model, config=config)
 
     t = time.time()
-    global_step, best_val_loss, best_val_acc = trainer.train()
+    global_step, best_val_loss = trainer.train()
     train_time = time.time() - t
 
     print()
     print("Training Finished.")
-    print("** Time: {.3f}".format(train_time))
+    print("** Time: {:.3f}".format(train_time))
     print("** Total Step: {}".format(global_step))
-    print("** Best Validation Loss: {.3f}".format(best_val_loss))
-    print("** Best Validation Accuracy: {.3f}%".format(best_val_acc))
+    print("** Best Validation Loss: {:.3f}".format(best_val_loss))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=711)
-    parser.add_argument("--path", type=str, default="./brave_data_label.csv")  ##
+    parser.add_argument(
+        "--path", type=str, default="./preprocessing/brave_data_label.csv"
+    )
     parser.add_argument("--ckpt_path", type=str, default="./checkpoints/")
+    parser.add_argument("--num_classes", type=int, default=1)
     parser.add_argument("--lr", type=float, default=0.01)
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument("--weight_decay", type=float, default=1e-3)
     parser.add_argument("--optimizer", type=str, default="sgd")
-    parser.add_argument("--epoch", type=int, default=50)
-    parser.add_argument("--eval_step", type=int, default=20)
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--eval_step", type=int, default=50)
 
     args = parser.parse_args()
     main(args)
--- trainer.py
+++ trainer.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 from typing import Tuple
 
 import torch
@@ -26,8 +26,8 @@ class Trainer:
             self.optimizer = optim.SGD(
                 self.model.parameters(),
                 lr=self.lr,
-                momentum=self.momentum,
-                weight_decay=self.weight_decay,
+                momentum=config["momentum"],
+                weight_decay=config["weight_decay"],
             )
         self.lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(
             self.optimizer, mode="min", factor=0.1, patience=20
@@ -39,92 +39,82 @@ class Trainer:
         self.path = config["csv_path"]
         self.train_loader, self.val_loader, self.test_loader = self.get_dataloader()
 
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
+        # self.logger = logging.getLogger(__name__)
+        # self.logger.setLevel(logging.INFO)
         self.save_path = config["ckpt_path"]
-        self.writer = SummaryWriter()
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+        self.writer = SummaryWriter(self.save_path)
         self.global_step = 0
         self.eval_step = config["eval_step"]
         self.best_val_loss = 1e8
-        self.best_val_acc = 0
 
     def get_dataloader(self) -> Tuple[DataLoader]:
-        return get_dataloader(self.path, iid=True)
+        return get_dataloader(self.path, iid=False)
 
-    def train(self):
-        for epoch in tqdm(self.epochs, desc="epoch"):
+    def train(self) -> Tuple[int, float]:
+        for epoch in tqdm(range(self.epochs), desc="epoch"):
             result = self._train_epoch(epoch)
 
         self.writer.close()
-        return self.global_step, self.best_val_loss, self.best_val_acc
+        return self.global_step, self.best_val_loss
 
-    def _train_epoch(self, epoch: int):
+    def _train_epoch(self, epoch: int) -> None:
         train_loss = 0.0
-        train_acc = 0.0
 
         self.model.train()
-        for step, batch in tqdm(enumerate(self.train_loader, desc="steps")):
+        for step, batch in tqdm(
+            enumerate(self.train_loader), desc="steps", total=len(self.train_loader)
+        ):
             img, label = map(lambda x: x.to(self.device), batch)
 
             output = self.model(img)
-            pred = None  ##
-            batch_acc = None  ##
 
             self.optimizer.zero_grad()
-            loss = self.criterion(output, label)
+            loss = self.criterion(output.squeeze(), label)
             loss.backward()
             self.optimizer.step()
 
             train_loss += loss.item()
-            # train_acc += batch_acc.item()
-
             self.global_step += 1
-
             if self.global_step % self.eval_step == 0:
                 tqdm.write(
-                    "global step: {:3}, train loss: {.3f}, train acc: {.3f}, lr: {.3f}".format(
-                        self.global_step, loss.item(), batch_acc.item(), self.lr
+                    "global step: {}, train loss: {:.3f}, lr: {:.6f}".format(
+                        self.global_step, loss.item(), self.lr
                     )
                 )
 
         train_loss /= step + 1
-        train_acc /= step + 1
+        val_loss = self._valid_epoch(epoch)
 
-        val_loss, val_acc = self._valid_epoch(epoch)
-
-        self.writer.add_scalars("loss", {"val": val_loss, "train": train_loss}, self.global_step)
-        self.writer.add_scalars("accuracy", {"val": val_acc, "train": train_acc}, self.global_step)
+        self.writer.add_scalars(
+            "loss", {"val": val_loss, "train": train_loss}, self.global_step
+        )
 
         tqdm.write(
-            "global step: {:3}, val loss: {.3f}, val acc: {.3f}".format(
-                self.global_step, val_loss, val_acc
-            )
+            "** global step: {}, val loss: {:.3f}".format(self.global_step, val_loss)
         )
 
         if val_loss < self.best_val_loss:
-            name = "./best_model.ckpt"
+            name = "best_model.ckpt"
             torch.save(self.model.state_dict(), os.path.join(self.save_path, name))
             self.best_val_loss = val_loss
-            self.best_val_acc = val_acc
 
     def _valid_epoch(self, epoch: int) -> Tuple[float]:
         val_loss = 0.0
-        val_acc = 0.0
 
         self.model.eval()
         with torch.no_grad():
-            for step, batch in tqdm(enumerate(self.val_loader), desc="val steps"):
+            for step, batch in tqdm(
+                enumerate(self.val_loader), desc="val steps", total=len(self.val_loader)
+            ):
                 img, label = map(lambda x: x.to(self.device), batch)
 
                 output = self.model(img)
                 loss = self.criterion(output, label)
-                batch_acc = None  ##
 
                 val_loss += loss.item()
-                val_acc += batch_acc.item()
 
         val_loss /= step + 1
-        val_acc /= step + 1
 
-        return val_loss, val_acc
-
+        return val_loss
