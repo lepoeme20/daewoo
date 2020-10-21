dd21da9faea10fdd9507df246eccc9587275fa13
lepoeme20 <lepoeme20@gmail.com>
2020-10-22 00:22:22 +0900

    example how to use dataloader

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import torchvision
 from utils.build_dataloader import get_dataloader
 
 csv_path = './brave_data_label.csv'
-trn_loader, dev_loader, tst_loader = get_dataloader(csv_path, iid=True)
+trn_loader, dev_loader, tst_loader = get_dataloader(
+    csv_path,
+    batch_size=32,
+    iid=True,
+    transform=0)
 
 img, label = next(iter(trn_loader))
