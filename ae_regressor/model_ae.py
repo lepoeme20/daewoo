6b9d1176d9d3841cdd003441bd0ef0ec84398321
lepoeme20 <lepoeme20@gmail.com>
2020-10-26 16:41:53 +0900

    add linear AE

--- ae_regressor/model_ae.py
+++ ae_regressor/model_ae.py
@@ -1,8 +1,17 @@
+"""
+변경: 기존 코드 -> CAE
+추가: AE (flatten)
+
+팁:
+    - 최근에는 ConvTranspose2d보다 Upsample + Conv2d를 더 많이 사용하는 추세
+    - norm을 사용하는 경우, pixel의 범위가 [-5.181, 10.275] (0.3352, 0.0647 인 경우)로 bound
+      되기 때문에 decoder의 마지막 레이어를 잘 고려해야 함
+"""
 import torch.nn as nn
 
-class Autoencoder(nn.Module):
+class CAE(nn.Module):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super(CAE, self).__init__()
         # Input size: [batch, 3, 32, 32]
         # Output size: [batch, 3, 32, 32]
 
@@ -13,8 +22,8 @@ class Autoencoder(nn.Module):
             nn.ReLU(),
             nn.Conv2d(24, 48, 10, stride=1, padding=2),
             nn.ReLU(),
-
         )
+
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(48, 24, 10, stride=1, padding=2),
             nn.ReLU(),
@@ -27,4 +36,30 @@ class Autoencoder(nn.Module):
     def forward(self, x):
         encoded = self.encoder(x)  # encode.shape = torch.Size([32, 1, 17, 17])
         decoded = self.decoder(encoded) # decode.shape = torch.Size([32, 48, 17, 17])
+        return encoded, decoded
+
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(1028, 128),
+            nn.LeakyReLU(True),
+            nn.Linear(128, 64),
+            nn.LeakyReLU(True),
+            nn.Linear(64, 32),
+            nn.LeakyReLU(True)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(32, 64),
+            nn.LeakyReLU(True),
+            nn.Linear(64, 128),
+            nn.LeakyReLU(True),
+            nn.Linear(128, 1028)
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
         return encoded, decoded
