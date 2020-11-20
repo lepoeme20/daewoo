ebde00b60726c5b2c7d7f69ec802c7e7f6acadaa
lepoeme20 <lepoeme20@gmail.com>
2020-11-21 02:18:04 +0900

    hotfix

--- utils/build_dataloader.py
+++ utils/build_dataloader.py
@@ -10,7 +10,9 @@ def get_dataloader(csv_path, batch_size, dtype, iid=False, transform=2, img_size
         # i.i.d condition
         unique_id = np.unique(df['label_idx'])
         np.random.shuffle(unique_id)
-        trn_idx, dev_idx, tst_idx = np.split(unique_id, [int(.6*len(df)), int(.8*len(df))])
+        trn_idx, dev_idx, tst_idx = np.split(
+            unique_id, [int(.6*len(unique_id)), int(.8*len(unique_id))]
+            )
         trn = df.loc[df['label_idx'].isin(trn_idx)]
         dev = df.loc[df['label_idx'].isin(dev_idx)]
         tst = df.loc[df['label_idx'].isin(tst_idx)]
