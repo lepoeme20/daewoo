5038bfe3e6b0dccedb63cc0d93d4259cb166c42c
lepoeme20 <lepoeme20@gmail.com>
2021-01-28 11:49:47 +0900

    remove change img path in csv

--- cnn_regressor/oned.py
+++ cnn_regressor/oned.py
@@ -226,7 +226,6 @@ class Trainer:
     def MAPE(self, pred, true):
         return torch.mean((pred - true).abs() / (true.abs() + 1e-8)) * 100
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--trn-dataset", type=str, default="weather", help="csv file path")
@@ -291,16 +290,6 @@ if __name__ == "__main__":
     elif args.label_type == 2:
         print(" Set label as period")
         args.label_type = "period"
-
-    print("Modifying img path in csv file...")
-    label_df = pd.read_csv(args.csv_path)
-    col_img_pth = label_df["image"]
-    folder_idx = col_img_pth[0].find("use_data")  # 'data_crop' is shared folder name
-    pth_before = col_img_pth[0][:folder_idx]
-    assert args.root_img_path[-1] == "/", "이미지 경로 마지막을 / 으로 끝나게 해 주세요~!"
-    label_df["image"] = label_df["image"].str.replace(pth_before, args.root_img_path)
-    label_df.to_csv(args.csv_path, index=False)
-    print("Done!")
 
     # Set random seed for reproducibility
     SEED = args.seed
