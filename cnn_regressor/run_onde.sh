9535254ea349890c0281c45aed7ae8a32d5d81b7
lepoeme20 <lepoeme20@gmail.com>
2021-01-28 15:37:52 +0900

    bash file for training

---
+++ cnn_regressor/run_onde.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dataset='weather_4'
+stride_tuple=(1 5 10)
+iid_tuple=(1 2)
+
+for iid in ${iid_tuple[@]}
+do
+    for stride in ${stride_tuple[@]}
+    do
+        python cnn_regressor/oned.py --epochs 100 --norm-type 2 --batch-size 512 --label-type 0 --root-img-path /media/lepoeme20/Data/projects/daewoo/ --trn-dataset $dataset --iid $iid --stride $stride
+    done
+done
