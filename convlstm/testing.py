13455979b49df384d5f3d60b50d3f790c1aff11a
hj-choi-95 <61812821+hj-choi-95@users.noreply.github.com>
2021-02-13 15:14:32 +0900

    add option of use_time_phase

--- convlstm/testing.py
+++ convlstm/testing.py
@@ -23,13 +23,18 @@ def main(args):
         folder_name += '_split_{}'.format(args.img_split_type)
         source_data = 'total_phase'
     else:
-        folder_name = 'result_source_'
-        if args.use_weather_1:
-            folder_name += '1'
-        if args.use_weather_4:
-            folder_name += '4'
-        folder_name += '_split_{}_seed_{}'.format(args.img_split_type, args.iid)
-        source_data = folder_name.split('source_')[-1].split('_split')[0]
+        if args.use_time_phase:
+            folder_name = 'result_source_time_phase'
+            folder_name += '_split_{}'.format(args.img_split_type)
+            source_data = 'time_phase'
+        else:
+            folder_name = 'result_source_'
+            if args.use_weather_1:
+                folder_name += '1'
+            if args.use_weather_4:
+                folder_name += '4'
+            folder_name += '_split_{}_seed_{}'.format(args.img_split_type, args.iid)
+            source_data = folder_name.split('source_')[-1].split('_split')[0]
     
     model = ConvLSTMModel(args.mem_size, args.img_split_type)
     model.load_state_dict(torch.load(os.path.join(args.save_root_path, folder_name, 'best_model.pt')))
@@ -39,30 +44,39 @@ def main(args):
         _, _, tst_loader_14_label = get_dataloader(False, args.weather_1_csv_path, args.weather_1_root_img_path,
                                                    False, args.weather_4_csv_path, args.weather_4_root_img_path,
                                                    True, args.weather_total_phase_csv_path,
-                                                   args.img_split_type, args.iid, args.batch_size)
+                                                   False, args.img_split_type, args.iid, args.batch_size)
         
         data_loaders = {'total_phase': tst_loader_14_label}
         results = {'total_phase': {'trues': np.array([]), 'preds': np.array([])}}
     else:
-        _, _, tst_loader_1 = get_dataloader(True, args.weather_1_csv_path, args.weather_1_root_img_path,
-                                            False, args.weather_4_csv_path, args.weather_4_root_img_path,
-                                            False, args.weather_total_phase_csv_path,
-                                            args.img_split_type, args.iid, args.batch_size)
-        # 기상 1호 4차 데이터 로더
-        _, _, tst_loader_4 = get_dataloader(False, args.weather_1_csv_path, args.weather_1_root_img_path,
-                                            True, args.weather_4_csv_path, args.weather_4_root_img_path,
-                                            False, args.weather_total_phase_csv_path,
-                                            args.img_split_type, args.iid, args.batch_size)
-        # 기상 1호 1차 & 4차 데이터 로더
-        _, _, tst_loader_14 = get_dataloader(True, args.weather_1_csv_path, args.weather_1_root_img_path,
-                                            True, args.weather_4_csv_path, args.weather_4_root_img_path,
-                                            False, args.weather_total_phase_csv_path,
-                                            args.img_split_type, args.iid, args.batch_size)
+        if args.use_time_phase:
+            _, _, tst_loader_time = get_dataloader(args.use_weather_1, args.weather_1_csv_path, args.weather_1_root_img_path,
+                                                   args.use_weather_4, args.weather_4_csv_path, args.weather_4_root_img_path,
+                                                   False, args.weather_total_phase_csv_path,
+                                                   True, args.img_split_type, args.iid, args.batch_size)
         
-        data_loaders = {'weather_1': tst_loader_1, 'weather_4': tst_loader_4, 'weather_14': tst_loader_14}
-        results = {'weather_1': {'trues': np.array([]), 'preds': np.array([])},
-                   'weather_4': {'trues': np.array([]), 'preds': np.array([])},
-                   'weather_14': {'trues': np.array([]), 'preds': np.array([])}}
+            data_loaders = {'time_phase': tst_loader_time}
+            results = {'total_phase': {'trues': np.array([]), 'preds': np.array([])}}
+        else:
+            _, _, tst_loader_1 = get_dataloader(True, args.weather_1_csv_path, args.weather_1_root_img_path,
+                                                False, args.weather_4_csv_path, args.weather_4_root_img_path,
+                                                False, args.weather_total_phase_csv_path,
+                                                False, args.img_split_type, args.iid, args.batch_size)
+            # 기상 1호 4차 데이터 로더
+            _, _, tst_loader_4 = get_dataloader(False, args.weather_1_csv_path, args.weather_1_root_img_path,
+                                                True, args.weather_4_csv_path, args.weather_4_root_img_path,
+                                                False, args.weather_total_phase_csv_path,
+                                                False, args.img_split_type, args.iid, args.batch_size)
+            # 기상 1호 1차 & 4차 데이터 로더
+            _, _, tst_loader_14 = get_dataloader(True, args.weather_1_csv_path, args.weather_1_root_img_path,
+                                                True, args.weather_4_csv_path, args.weather_4_root_img_path,
+                                                False, args.weather_total_phase_csv_path,
+                                                False, args.img_split_type, args.iid, args.batch_size)
+
+            data_loaders = {'weather_1': tst_loader_1, 'weather_4': tst_loader_4, 'weather_14': tst_loader_14}
+            results = {'weather_1': {'trues': np.array([]), 'preds': np.array([])},
+                       'weather_4': {'trues': np.array([]), 'preds': np.array([])},
+                       'weather_14': {'trues': np.array([]), 'preds': np.array([])}}
 
     print('==> Start Testing')
     for target_data in data_loaders:
@@ -99,9 +113,10 @@ if __name__ == '__main__':
     parser.add_argument('--iid', type=int, default=0)
     parser.add_argument('--img_split_type', type=int, default=0,
                         help='0: img to 3 frames vertically | 1: img to 5 frames vertically | 2: img to 3 frames horizontally')
-    parser.add_argument('--use-weather-1', type=bool, default=False)
+    parser.add_argument('--use-weather-1', type=bool, default=True)
     parser.add_argument('--use-weather-4', type=bool, default=False)
-    parser.add_argument('--use-total-phase', type=bool, default=True)
+    parser.add_argument('--use-total-phase', type=bool, default=False)
+    parser.add_argument('--use-time-phase', type=bool, default=True)
     parser.add_argument('--weather-1-csv-path', type=str,
                         default='/media/heejeong/HDD2/project/daewoo/data/weather_1/wave_radar/weather_1_data_label_seed.csv',
                         help='Csv file directory of labels for weather1')
